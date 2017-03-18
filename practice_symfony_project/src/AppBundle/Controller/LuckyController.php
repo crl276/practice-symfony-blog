aa14ad031d43992fc5e54c37725d4c2b59d2e877
Callan Lamb <callan.lamb@gmail.com>
2017-03-18 19:07:31 -0400

    Add public function numberAction()

--- practice_symfony_project/src/AppBundle/Controller/LuckyController.php
+++ practice_symfony_project/src/AppBundle/Controller/LuckyController.php
@@ -5,3 +5,15 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+class LuckyController
+{
+	public function numberAction()
+	{
+		$number = mt_rand(0, 100);
+
+		return new Response(
+			'<html><body>Lucky Number: ' .$number.'</body></html>'
+			);
+	}
+}
