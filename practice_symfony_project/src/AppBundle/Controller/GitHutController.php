a7174d29040cd6ec7c50064b6e5b2e638daad2b8
Callan Lamb <callan.lamb@gmail.com>
2017-03-19 19:01:26 -0400

    Add githutAction function

--- practice_symfony_project/src/AppBundle/Controller/GitHutController.php
+++ practice_symfony_project/src/AppBundle/Controller/GitHutController.php
@@ -5,3 +5,15 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+class GitHutController extends Controller
+{
+
+	/**
+	* @Route("/", name="githut")
+	*/
+	public function githutAction(Request $request)
+	{
+		return $this->render('githut.index.html.twig');
+	}
+}
