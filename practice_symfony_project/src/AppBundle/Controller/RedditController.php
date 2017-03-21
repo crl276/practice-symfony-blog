bced06c877b51b69f11b3eb3b015c99d010b8c97
Callan Lamb <callan.lamb@gmail.com>
2017-03-21 09:38:25 -0400

    Add relation db and Reddit post

---
+++ practice_symfony_project/src/AppBundle/Controller/RedditController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RedditController extends Controller
+{
+	/**
+	* @Route("/", name="list")
+	*/
+	public function listAction()
+	{
+
+		$posts = $this->getDoctrine()->getRepository('AppBundle:RedditPost')->findAll();
+
+
+
+		return $this->render('/reddit/index.html.twig', [
+			'posts' => $posts
+		]);
+	}
+}
