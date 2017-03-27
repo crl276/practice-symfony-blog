113c2f9c7ffd5e8996f7e4bb87ac495b1945f793
Callan Lamb <callan.lamb@gmail.com>
2017-03-26 21:50:21 -0400

    Add updateAction function

--- practice_symfony_project/src/AppBundle/Controller/RedditController.php
+++ practice_symfony_project/src/AppBundle/Controller/RedditController.php
@@ -26,14 +26,39 @@ class RedditController extends Controller
 	*/
 	public function createAction($test)
 	{
+
+//Fetch the EntityManager from Doctrine ->getManager()
 		$em = $this->getDoctrine()->getManager();
-
+//create a new instance of the RedditPost entity that we created. An entity is a plan old PHP object class with
+//specific Doctrine metadata (the annotations that we use in RedditPost.php) telling Doctrine how the db tables
+//fields match up to the class properties.
 		$post = new RedditPost();
+//Once we new'd up the RedditPost, we can access the setTitle method we added. This sets the $title to hello . $test
 		$post->setTitle('hello' . $test);
-
+//This uses the persist method on getManager() to save (or persist) the data to the db.
 		$em->persist($post);
+//persist just tells Doctrine that they want to queue up the change. Flush is what you need to do to
+//actually save these changes. flush is what actually changes it
 		$em->flush();
 
+//Redirct to our previous defined route
+		return $this->redirectToRoute('list');
+	}
+
+	public function updateAction($id, $text)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+		$post = $em->getRepository('AppBundle:RedditPost')->find($id);
+
+		if(!$post) {
+			throw $this->createNotFoundException('that\'s not a record');
+		}
+
+		/** @var $post RedditPost */
+		$post->setTitle('updated title ' . $text);
+
+		$em->flush();
 
 		return $this->redirectToRoute('list');
 	}
